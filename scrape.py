6e02e01f54893a0cf219db023a160bf74802f845
Tan Yong Sheng <tys203831@gmail.com>
2024-02-03 09:33:07 +0800

    Create scrape.py

---
+++ scrape.py
@@ -0,0 +1,15 @@
+# scrape blog from tanyongsheng.net 
+# idea: combine with github actions to update README.md file with new blog article release
+
+import requests
+from urllib.parse import urljoin
+from datetime import datetime
+
+blog_url = "https://tanyongsheng.net"
+response = requests.get(urljoin(blog_url, "/wp-json/wp/v2/posts/"))
+data = response.json()
+
+
+for idx in len(data):
+    title = data[idx]["title"]["rendered"]
+    date = datetime.strptime(response.json()[0]["date"], "%d/%m/%y")
